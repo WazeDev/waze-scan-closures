2281de92bff56c3465c8a2c0600452dc1586269f
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-06-18 16:27:20 -0500

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -15,19 +15,50 @@
 
 (function() {
     'use strict';
-     window.SDK_INITIALIZED.then(initialize);
+    window.SDK_INITIALIZED.then(init);
     let sdk;
-    function initalize() {
-    sdk = window.getWmeSdk({
-        scriptId: 'wme-scan-closures',
-        scriptName: 'Waze Scan Closures'
-    });
-    // set zoom level to 14
-    sdk.Map.setZoomLevel({zoomLevel: 14})
+    let userReportedClosures = [];
+    let url = "";
+
+    function init() {
+        sdk = window.getWmeSdk({
+            scriptId: 'wme-scan-closures',
+            scriptName: 'Waze Scan Closures'
+        });
+        sdk.Events.on({
+            eventName: "wme-map-data-loaded",
+            eventHandler: updateRoadClosures
+        })
+        userReportedClosures = filterUserClosures(sdk.DataModel.RoadClosures.getAll());
+        console.log(`Waze Scan Closures: Initalized!`);
     }
-    function scan() {
-    // wait till ready
-    //sdk.State.isReady();
+    function filterUserClosures(closures) {
+    return closures.filter(c =>
+        !c.description &&
+        c.startDate &&
+        c.endDate &&
+        (new Date(c.endDate).getTime() - new Date(c.startDate).getTime()) === 3600000
+      );
+    }
+
+    function updateRoadClosures() {
+        let currentUserReportedClosures = filterUserClosures(sdk.DataModel.RoadClosures.getAll());
+        if (userReportedClosures.length !== currentUserReportedClosures.length) {
+            userReportedClosures = currentUserReportedClosures;
+            console.log(`Waze Scan Closures: Found ${userReportedClosures.length} user reported closures!`)
+            console.log(userReportedClosures)
+            //sendClosures();
+        }
+    }
+
+    function sendClosures() {
+        let ClosuresReq = fetch(url, {
+            method: "POST",
+            body: JSON.stringify(roadClosures),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
     }
     // Your code here...
 })();
